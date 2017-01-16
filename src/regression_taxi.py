7034f6a6dbec4206454740f61a4d0da2fe12da68
Shijia E <e.shijia@gmail.com>
2017-01-16 12:44:30 +0800

    bug fix

--- src/regression_taxi.py
+++ src/regression_taxi.py
@@ -25,11 +25,17 @@ adam = Adam(lr=0.0001)
 
 
 def load_dataset():
-    train_dataframe = pandas.read_csv(os.path.join(data_path, 'train.dat'), header=None)
+    train_dataframe = pandas.read_csv(os.path.join(data_path, 'train1.dat'), header=None)
+    train1_dataframe = pandas.read_csv(os.path.join(data_path, 'train1.dat'), header=None)
+    train2_dataframe = pandas.read_csv(os.path.join(data_path, 'train2.dat'), header=None)
     test_dataframe = pandas.read_csv(os.path.join(data_path, 'test.dat'), header=None)
+    valid_dataframe = pandas.read_csv(os.path.join(data_path, 'valid.dat'), header=None)
     train_dataset = train_dataframe.values.astype('float32')
+    train1_dataset = train1_dataframe.values.astype('float32')
+    train2_dataset = train2_dataframe.values.astype('float32')
     test_dataset = test_dataframe.values.astype('float32')
-    return train_dataset, test_dataset
+    valid_dataset = valid_dataframe.values.astype('float32')
+    return train_dataset, train1_dataset, train2_dataset, test_dataset, valid_dataset
 
 
 def load_samples():
@@ -50,17 +56,14 @@ def maxout_model():
     return model
 
 
-# def ae():
-#     encoding_dim = 5
-#     input_seq = Input(shape=(22,))
-#     encoded = Dense(encoding_dim, activation='relu')(input_seq)
-#     decoded = Dense(22, activation='sigmoid')(encoded)
-#     autoencoder = Model(input=input_seq, output=decoded)
-#
-#     encoder = Model(input=input_seq, output=encoded)
-#
-#     autoencoder.compile(optimizer=adam, loss='mse')
-#     return autoencoder, encoder
+def mlp_model():
+    model = Sequential()
+    model.add(Dense(128, init='glorot_normal', input_dim=22, activation='relu'))
+    model.add(Dense(64, init='glorot_normal', activation='relu'))
+    model.add(Dense(32, init='glorot_normal', activation='relu'))
+    model.add(Dense(1, init='zero'))
+    model.compile(loss='mape', optimizer='adam')
+    return model
 
 
 def ae():
@@ -72,19 +75,76 @@ def ae():
     return model
 
 
-def make_submit_maxout():
-    train, test = load_dataset()
+def make_submit_mlp():
+    train, train1, train2, test, valid = load_dataset()
+
     x_train = train[:, 0:22]
     y_train = train[:, 22]
 
+    x_train1 = train1[:, 0:22]
+    y_train1 = train1[:, 22]
+
+    x_train2 = train2[:, 0:22]
+    y_train2 = train2[:, 22]
+
+    x_valid = valid[:, 0:22]
+    y_valid = valid[:, 22]
+
     x_test = test[:, 0:22]
+
+    x_scaler = MinMaxScaler(feature_range=(0, 1)).fit(x_train)
+    y_scaler = MinMaxScaler(feature_range=(0, 1)).fit(y_train)
+    x_train1 = (x_scaler.transform(x_train1.reshape(-1, 22)))
+    y_train1 = (y_scaler.transform(y_train1.reshape(-1, 1)))
+    x_valid = (x_scaler.transform(x_valid.reshape(-1, 22)))
+    y_valid = (y_scaler.transform(y_valid.reshape(-1, 1)))
+    # x_test = (x_scaler.fit_transform(x_test.reshape(-1, 22)))
+    proposed_model1 = mlp_model()
+    proposed_model1.fit(x_train1, y_train1, nb_epoch=10, batch_size=128, verbose=1, validation_data=(x_valid, y_valid))
+    proposed_model2 = mlp_model()
+    proposed_model2.fit(x_train2, y_train2, nb_epoch=10, batch_size=128, verbose=1, validation_data=(x_valid, y_valid))
+
+    y_predict = list()
+    for test_sample in x_test:
+        if test_sample[20] > 12065.31:
+            test_sample = x_scaler.transform(test_sample.reshape(-1, 22))
+            sample_result = proposed_model2.predict(test_sample, batch_size=1)
+            sample_predict = y_scaler.inverse_transform(sample_result.reshape(-1, 1))
+            y_predict.extend(sample_predict)
+        else:
+            test_sample = x_scaler.transform(test_sample.reshape(-1, 22))
+            sample_result = proposed_model1.predict(test_sample, batch_size=1)
+            sample_predict = y_scaler.inverse_transform(sample_result.reshape(-1, 1))
+            y_predict.extend(sample_predict)
+
+    y_predict = np.asarray(y_predict)
+
+    trip_id = np.array(range(1, len(y_predict)+1))
+    results = np.column_stack((trip_id, y_predict))
+    timestamp = time.strftime(time_format, time.gmtime(time.time()))
+    np.savetxt('rst_' + timestamp + '.csv', results, header='pathid,time', comments='', fmt='%d,%f')
+
+
+def make_submit_maxout():
+    train, test, valid = load_dataset()
+    x_train = train[:, 0:22]
+    y_train = train[:, 22]
+
+    x_valid = valid[:, 0:22]
+    y_valid = valid[:, 22]
+
+    x_test = test[:, 0:22]
+
     x_scaler = MinMaxScaler(feature_range=(0, 1))
     y_scaler = MinMaxScaler(feature_range=(0, 1))
     x_train = (x_scaler.fit_transform(x_train.reshape(-1, 22)))
     y_train = (y_scaler.fit_transform(y_train.reshape(-1, 1)))
+    x_valid = (x_scaler.fit_transform(x_valid.reshape(-1, 22)))
+    y_valid = (y_scaler.fit_transform(y_valid.reshape(-1, 1)))
     x_test = (x_scaler.fit_transform(x_test.reshape(-1, 22)))
+
     proposed_model = maxout_model()
-    proposed_model.fit(x_train, y_train, nb_epoch=5, batch_size=128, verbose=1)
+    proposed_model.fit(x_train, y_train, nb_epoch=5, batch_size=128, verbose=1, validation_data=(x_valid, y_valid))
     y_predict = y_scaler.inverse_transform(proposed_model.predict(x_test).reshape(-1, 1))
 
     trip_id = np.array(range(1, len(y_predict)+1))
@@ -94,15 +154,21 @@ def make_submit_maxout():
 
 
 def make_submit_ae_mlp():
-    train, test = load_dataset()
+    train, test, valid = load_dataset()
     x_train = train[:, 0:22]
     y_train = train[:, 22]
 
+    x_valid = valid[:, 0:22]
+    y_valid = valid[:, 22]
+
     x_test = test[:, 0:22]
+
     x_scaler = MinMaxScaler(feature_range=(0, 1))
     y_scaler = MinMaxScaler(feature_range=(0, 1))
     x_train = (x_scaler.fit_transform(x_train.reshape(-1, 22)))
     y_train = (y_scaler.fit_transform(y_train.reshape(-1, 1)))
+    x_valid = (x_scaler.fit_transform(x_valid.reshape(-1, 22)))
+    y_valid = (y_scaler.fit_transform(y_valid.reshape(-1, 1)))
     x_test = (x_scaler.fit_transform(x_test.reshape(-1, 22)))
 
     autoencoder = ae()
@@ -119,7 +185,7 @@ def make_submit_ae_mlp():
     model.add(Dense(1, init='zero', activation='linear'))
 
     model.compile(optimizer='adam', loss='mape')
-    model.fit(x_train, y_train, nb_epoch=5, batch_size=128, verbose=1)
+    model.fit(x_train, y_train, nb_epoch=5, batch_size=128, verbose=1, validation_data=(x_valid, y_valid))
     y_predict = y_scaler.inverse_transform(model.predict(x_test).reshape(-1, 1))
 
     trip_id = np.array(range(1, len(y_predict)+1))
@@ -128,20 +194,7 @@ def make_submit_ae_mlp():
     np.savetxt('rst_' + timestamp + '.csv', results, header='pathid,time', comments='', fmt='%d,%f')
 
 
-def model_evaluation():
-    samples = load_samples()
-    x_samples = samples[:, 0:22]
-    y_samples = samples[:, 22]
-    proposed_model = model_wrapper()
-    x_train, x_test, y_train, y_test = train_test_split(x_samples, y_samples, test_size=0.1, random_state=seed)
-    proposed_model.fit(x_train, y_train)
-    score = proposed_model.score(x_test, y_test)
-    print '\n'
-    print ('Standardized: %.4f MAPE' % score)
-    print '\n'
-
-
 if __name__ == '__main__':
-    # model_evaluation()
-    make_submit_maxout()
+    # make_submit_maxout()
     # make_submit_ae_mlp()
+    make_submit_mlp()
