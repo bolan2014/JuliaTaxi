873dfc33f4f9905183e739c582d04b9748ce5b18
Shijia E <e.shijia@gmail.com>
2017-01-17 17:44:56 +0800

    bug fix

--- src/regression_taxi.py
+++ src/regression_taxi.py
@@ -54,7 +54,12 @@ def maxout_model():
 
 def mlp_model():
     model = Sequential()
-    model.add(Dense(128, init='glorot_normal', activation='relu', input_dim=21))
+    model.add(Dense(256, init='glorot_normal', activation='relu', input_dim=21))
+    model.add(Dense(128, init='glorot_normal', activation='relu'))
+    model.add(Dense(128, init='glorot_normal', activation='relu'))
+    model.add(Dense(128, init='glorot_normal', activation='relu'))
+    model.add(Dense(128, init='glorot_normal', activation='relu'))
+    model.add(Dense(128, init='glorot_normal', activation='relu'))
     model.add(Dense(64, init='glorot_normal', activation='relu',))
     model.add(Dense(32, init='glorot_normal', activation='relu',))
     model.add(Dense(1, init='zero'))
