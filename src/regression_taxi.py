d371246e012bb443e8e52f2062cd320a0e87e763
Shijia E <e.shijia@gmail.com>
2017-01-17 22:12:13 +0800

    tmd

--- src/regression_taxi.py
+++ src/regression_taxi.py
@@ -91,10 +91,10 @@ def make_submit_mlp():
             train4.append(train_sample)
         elif 5000 >= train_sample[19] > 4000:
             train3.append(train_sample)
-        elif 4000 >= train_sample[19] > 2749:
+        elif 4000 >= train_sample[19] > 2748.5:
             train2.append(train_sample)
-        elif 2749 >= train_sample[19] > 2748:
-            print train_sample
+        # elif 2749 >= train_sample[19] > 2748:
+        #     print train_sample
         else:
             # for i in range(22):
             #     if not train_sample[i]:
