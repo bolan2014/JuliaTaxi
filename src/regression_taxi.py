5e6c5eb24b6c1777c2591a488e16514cfa022266
Shijia E <e.shijia@gmail.com>
2017-01-18 09:27:27 +0800

    bug fix

--- src/regression_taxi.py
+++ src/regression_taxi.py
@@ -229,7 +229,7 @@ def make_submit_maxout():
             continue
         valid.append(valid_sample)
 
-    train = np.asarray(train)[0:1100000, :]
+    train = np.asarray(train)[0:800000, :]
     valid = np.asarray(valid)
 
     x_train = train[:, 0:21]
