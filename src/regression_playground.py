d46b4ac0f07197fced5a1f6448706a6ae324dd43
Shijia E <e.shijia@gmail.com>
2017-01-30 21:02:33 +0800

    bug fix

--- src/regression_playground.py
+++ src/regression_playground.py
@@ -20,6 +20,13 @@ def mean_absolute_percentage_error(y_true, y_pred):
     return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
 
+def make_submit(model_name, y_pred):
+    trip_id = np.array(range(1, len(y_pred) + 1))
+    results = np.column_stack((trip_id, y_pred))
+    timestamp = time.strftime(time_format, time.gmtime(time.time()))
+    np.savetxt(model_name + '_' + timestamp + '.csv', results, header='pathid,time', comments='', fmt='%d,%f')
+
+
 train_dataframe = pandas.read_csv(os.path.join(data_path, 'train.dat'), header=None)
 test_dataframe = pandas.read_csv(os.path.join(data_path, 'test.dat'), header=None)
 valid_dataframe = pandas.read_csv(os.path.join(data_path, 'valid.dat'), header=None)
@@ -51,17 +58,22 @@ x_test = ss_X.transform(x_test)
 x_valid = ss_X.transform(x_valid)
 # y_valid = ss_y.transform(y_valid)
 
-# Random Forest
-rfr = RandomForestRegressor(n_estimators=50, random_state=seed, n_jobs=20)
-rfr.fit(x_train, y_train)
-rfr_y_predict = ss_y.inverse_transform(rfr.predict(x_valid))
+# # Random Forest
+# rfr = RandomForestRegressor(n_estimators=50, random_state=seed, n_jobs=20)
+# rfr.fit(x_train, y_train)
+# rfr_y_predict = ss_y.inverse_transform(rfr.predict(x_valid))
 
-print 'Begin to evaluate ...'
-print 'The MAPE value of Random Forest is', mean_absolute_percentage_error(y_valid, rfr_y_predict)
+# print 'The MAPE value of Random Forest is', mean_absolute_percentage_error(y_valid, rfr_y_predict)
 
-y_predict = ss_y.inverse_transform(rfr.predict(x_test))
+# y_rfr_predict = ss_y.inverse_transform(rfr.predict(x_test))
 
-trip_id = np.array(range(1, len(y_predict) + 1))
-results = np.column_stack((trip_id, y_predict))
-timestamp = time.strftime(time_format, time.gmtime(time.time()))
-np.savetxt('rst_' + timestamp + '.csv', results, header='pathid,time', comments='', fmt='%d,%f')
+# ExtraTrees Regressor
+etr = ExtraTreesRegressor(random_state=seed, n_estimators=50, n_jobs=20)
+etr.fit(x_train, y_train)
+etr_y_predict = ss_y.inverse_transform(etr.predict(x_valid))
+
+print 'The MAPE value of Extra Tree is', mean_absolute_percentage_error(y_valid, etr_y_predict)
+
+y_etr_predict = ss_y.inverse_transform(etr.predict(x_test))
+
+make_submit(etr, y_etr_predict)
