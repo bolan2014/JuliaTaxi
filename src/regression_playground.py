6bca281894dfacbf993353f0a721349c24848b7d
Shijia E <e.shijia@gmail.com>
2017-01-31 11:37:04 +0800

    bug fix

--- src/regression_playground.py
+++ src/regression_playground.py
@@ -8,6 +8,8 @@ from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor, GradientBoostingRegressor
 from sklearn.neighbors import KNeighborsRegressor
 from xgboost import XGBRegressor
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
 
 data_path = '../data'
 time_format = '%Y-%m-%d_%X'
@@ -35,15 +37,6 @@ train_dataset = train_dataframe.values.astype('float32')
 test_dataset = test_dataframe.values.astype('float32')
 valid_dataset = valid_dataframe.values.astype('float32')
 
-# train = list()
-# for index, train_sample in enumerate(train_dataset):
-#     if not train_sample[21]:
-#         print index
-#         continue
-#     else:
-#         train.append(train_sample)
-#
-# train = np.asarray(train)
 
 x_train = train_dataset[:, 0:21]
 y_train = train_dataset[:, 21]
@@ -51,35 +44,50 @@ x_test = test_dataset[:, 0:21]
 x_valid = valid_dataset[:, 0:21]
 y_valid = valid_dataset[:, 21]
 
-ss_X = MinMaxScaler()
-ss_y = MinMaxScaler()
+# ss_X = StandardScaler()
+#
+# x_train = ss_X.fit_transform(x_train)
+# x_test = ss_X.transform(x_test)
+# x_valid = ss_X.transform(x_valid)
 
-x_train = ss_X.fit_transform(x_train)
-y_train = ss_y.fit_transform(y_train)
-x_test = ss_X.transform(x_test)
-x_valid = ss_X.transform(x_valid)
-# y_valid = ss_y.transform(y_valid)
+# # Random Forest
+# rfr = RandomForestRegressor(n_estimators=50, random_state=seed, n_jobs=20)
+# rfr.fit(x_train, y_train)
+# rfr_y_predict = ss_y.inverse_transform(rfr.predict(x_valid))
+# print 'The MAPE value of Random Forest is', mean_absolute_percentage_error(y_valid, rfr_y_predict)
+# y_rfr_predict = ss_y.inverse_transform(rfr.predict(x_test))
+# make_submit('random_forest', y_rfr_predict)
 
-# Random Forest
-rfr = RandomForestRegressor(n_estimators=50, random_state=seed, n_jobs=20)
-rfr.fit(x_train, y_train)
-rfr_y_predict = ss_y.inverse_transform(rfr.predict(x_valid))
-print 'The MAPE value of Random Forest is', mean_absolute_percentage_error(y_valid, rfr_y_predict)
-y_rfr_predict = ss_y.inverse_transform(rfr.predict(x_test))
-make_submit('random_forest', y_rfr_predict)
+# # ExtraTrees Regressor
+# etr = ExtraTreesRegressor(random_state=seed, n_estimators=50, n_jobs=20)
+# etr.fit(x_train, y_train)
+# etr_y_predict = etr.predict(x_valid)
+# print 'The MAPE value of Extra Tree is', mean_absolute_percentage_error(y_valid, etr_y_predict)
+# y_etr_predict = etr.predict(x_test)
+# make_submit('extremely_randomized_trees', y_etr_predict)
 
-# ExtraTrees Regressor
-etr = ExtraTreesRegressor(random_state=seed, n_estimators=50, n_jobs=20)
-etr.fit(x_train, y_train)
-etr_y_predict = ss_y.inverse_transform(etr.predict(x_valid))
-print 'The MAPE value of Extra Tree is', mean_absolute_percentage_error(y_valid, etr_y_predict)
-y_etr_predict = ss_y.inverse_transform(etr.predict(x_test))
-make_submit('extremely_randomized_trees', y_etr_predict)
+# # KNN
+# knr = KNeighborsRegressor(weights='distance', n_jobs=20)
+# knr.fit(x_train, y_train)
+# knr_y_predict = knr.predict(x_valid)
+# print 'The MAPE value of KNN is', mean_absolute_percentage_error(y_valid, knr_y_predict)
+# y_knr_predict = knr.predict(x_test)
+# make_submit('knn', y_knr_predict)
 
-# KNN
-knr = KNeighborsRegressor(weights='distance', n_jobs=20)
-knr.fit(x_train, y_train)
-knr_y_predict = ss_y.inverse_transform(knr.predict(x_valid))
-print 'The MAPE value of KNN is', mean_absolute_percentage_error(y_valid, knr_y_predict)
-y_knr_predict = ss_y.inverse_transform(knr.predict(x_test))
-make_submit('knn', y_knr_predict)
+reg = list()
+reg.append(('standardize', StandardScaler()))
+reg.append(('etr', ExtraTreesRegressor()))
+pipeline = Pipeline(reg)
+parameters = {'n_estimators': np.linspace(10, 100, num=10),
+              'max_features': ('auto', 'sqrt', 'log2', None),
+              'max_depth': [10, 20, 50, 100, None]}
+gs = GridSearchCV(pipeline, parameters, verbose=2, refit=True, cv=20, n_jobs=-1)
+gs.fit(x_train, y_train)
+print gs.best_params_
+print gs.best_score_
+
+gs_y_predict = gs.predict(x_valid)
+print 'The MAPE value of Grid Search is', mean_absolute_percentage_error(y_valid, gs_y_predict)
+
+y_gs_predict = gs.predict(x_test)
+make_submit('grid_search', y_gs_predict)
